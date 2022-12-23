c698a54615a61f6f31f95334022d731083e697b5
tanxuelee <xltan20@gmail.com>
2022-12-23 17:25:18 +0800

    register version 2

--- php/register.php
+++ php/register.php
@@ -27,7 +27,8 @@ echo '
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Register Page</title>
     <style>
         body {
@@ -53,6 +54,28 @@ echo '
                 document.getElementById('message').innerHTML = 'not matching';
             }
         }
+        
+        //function preview() {
+        //        frame.src = URL.createObjectURL(event.target.files[0]);
+        //}
+        
+        function password() {
+            var x = document.getElementById("user_password");
+            if (x.type === "password") {
+                x.type = "text";
+            } else {
+                x.type = "password";
+            }
+        }
+        
+        function password1() {
+            var x = document.getElementById("confirm_password");
+            if (x.type === "password") {
+                x.type = "text";
+            } else {
+                x.type = "password";
+            }
+        }
     </script>
 </head>
 <body>
@@ -60,6 +83,14 @@ echo '
         <form action="" id="form" method="post" class="rounded-3">
             <h1><strong>Registration Form</strong></h1>
             <hr><br>
+            <!--<div class="form-group row">
+                <label class="col-sm-2 col-form-label" for="profile_pic">Profile Picture:*</label>
+                <div class="col-10">
+                    <input class="form-control" type="file" name="profile_pic" accept="image/*" onchange="preview()" required>
+                    <br><img src="https://via.placeholder.com/200x200?text=No+Picture" id="frame" class="img-thumbnail" width="200" height="200">
+                </div>
+            </div>
+            <br>-->
             <div class="form-group row">
                 <label class="col-sm-2 col-form-label" for="name">Full Name:*</label>
                 <div class="col-10">
@@ -85,6 +116,8 @@ echo '
                 <label class="col-sm-2 col-form-label" for="password">Password:*</label>
                 <div class="col-10">
                     <input type="password" class="form-control" name="user_password" id="user_password" onkeyup='check();' placeholder="Your password" required>
+                    <br><input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" onclick="password()">
+                    <label class="form-check-label" for="flexCheckDefault">Show Password</label>
                 </div>
             </div>
             <br>
@@ -92,7 +125,9 @@ echo '
                 <label class="col-sm-2 col-form-label" for="password">Confirm Password:*</label>
                 <div class="col-10">
                     <input type="password" class="form-control" id="confirm_password" onkeyup='check();' placeholder="Confirm password" required>
-                    <span id='message'></span>
+                    <br><input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" onclick="password1()">
+                    <label class="form-check-label" for="flexCheckDefault">Show Confirm Password</label>
+                    <br><br><span id='message'></span>
                 </div>
             </div>
             <br>
@@ -105,15 +140,26 @@ echo '
             if (isset($_POST["submit"])) {
                 $user_name = $_POST['user_name'];
                 $user_email = $_POST['user_email'];
-                $user_password = $_POST['user_password'];
+                $user_password = sha1($_POST['user_password']);
                 $user_phone = $_POST['user_phone'];
-
+                //$base64image = $_POST['profile_pic'];
+                
                 $sqlinsert = "INSERT INTO tbl_users (user_name,user_email,user_password,user_phone) VALUES ('" . $user_name . "', '" . $user_email . "', '" . $user_password . "' , '" . $user_phone . "')";
                 if ($conn->query($sqlinsert) === TRUE) {
-                        echo "<script>alert('Registration Successful!');</script>";
+                    // $filename = mysqli_insert_id($conn);
+                    // $decoded_string = base64_decode($base64image);
+                    // $path = '../images/users/' . $filename . '.jpg';
+                    // $is_written = file_put_contents($path, $decoded_string);
+                    echo "<script>alert('Registration Successful!');
+                    location.replace('login.php');</script>";
                 }
             }
             ?>
+            
+            <br>
+            <div class="text-center">
+                <p class="mb-5 pb-lg-2" style="color: #393f81;">Already have an account? <a href="login.php" style="color: #393f81;"><b>Login now</b></a></p>
+            </div>
         </form>
     </div>
 </body>
